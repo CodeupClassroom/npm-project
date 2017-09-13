bbba2869e5611f3c866ef489df5053a288663870
Luis Montealegre <montealegreluis@gmail.com>
2017-09-13 12:08:37 -0500

    Include modules using require

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const $ = require("jquery"); // loading jquery.min.js
+const data = require("./data");
+const math = require("./math");
+
+console.log(math.add(10, 98));
+console.log(math.subtract(1, 98));
+console.log(math.multiply(2, 9));
+console.log(math.divide(10, 2.5));
+
+// similar to doing this
+/*const data = {
+    someNumber: 42,
+    whichSideOfTheForce: 'The Dark Side',
+    arnie: 'Get to da choppa!',
+};*/
 
 const hello = () => console.log("hello redwood from webpack!");
 
 hello();
+
+console.log(data.whichSideOfTheForce);
 
 $("body")
     .html("<h1>Hello from jQuery</h1>")
